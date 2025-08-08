6d323cc262f920ef806b924eaf9698f4af05b7cf
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-08-07 23:01:03 -0300

    adding plguins to example

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,14 @@
+import terser from '@rollup/plugin-terser';
+import filesize from 'rollup-plugin-filesize';
+
 export default {
   input: "src/main.js",
   output: {
     file: "dist/bundle.js",
     format: "esm",
   },
+  plugins: [
+    terser(),
+    filesize()
+  ]
 };
